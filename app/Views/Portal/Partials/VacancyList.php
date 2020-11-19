a7bd231c93c3100a1ef010bfd229536736e12f11
Wahyu Adi Candra <candraa6@gmail.com>
2020-11-20 00:44:42 +0700

    fix vacancylist styling in any container, not just relative to viewport

--- app/Views/Portal/Partials/VacancyList.php
+++ app/Views/Portal/Partials/VacancyList.php
@@ -26,7 +26,7 @@ class VacancyList extends BaseView
         <ul class="job-listings mb-3">
             
             <?php if (empty($this->vacancy)) : ?>
-                <li class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center justify-content-center" style="min-height: 150px;">
+                <li class="job-listing d-flex pb-3 pb-sm-0 align-items-center justify-content-center" style="min-height: 150px;">
                     <a class="primary"></a>
                     <h2>No Vacancy available</h2>
                 </li>
@@ -35,26 +35,27 @@ class VacancyList extends BaseView
 
                 <?php for ($i = 0; $i < $this->limit;  $i++) : ?>
                     <?php $vacancyItem = $this->vacancy[$i]; ?>
-                    <li class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center">
+                    <li class="job-listing job-listing-fix d-flex align-items-center">
 
                         <a class="primary" href="<?= route_to('detail', $vacancyItem['id']) ?>"></a>
 
 
-                        <div class="job-listing-logo d-flex align-items-center justify-content-center" style="height: 150px; width: 150px;">
-                            <img src="<?= $vacancyItem['image'] ?>" alt="Image" class="img-fluid">
+                        <div class="job-listing-logo job-listing-logo-fix">
+                            <div class="image-container d-flex align-items-center justify-content-center" style="height: 150px; width: 150px;">
+                                <img src="<?= $vacancyItem['image'] ?>" alt="Image" class="img-fluid">
+                            </div>
                         </div>
 
-                        <div class="job-listing-about d-sm-flex custom-width w-100 justify-content-between mx-4">
-                            <div class="job-listing-position custom-width w-50 mb-3 mb-sm-0">
+                        <div class="job-listing-about job-listing-about-fix d-sm-flex justify-content-between mx-4">
+                            <div class="job-listing-position flex-w-50 mb-3 mb-sm-0">
                                 <h2><?= $vacancyItem['profession'] ?></h2>
                                 <strong><?= $vacancyItem['company'] ?></strong>
-
                                 <h4>Test</h4>
                             </div>
-                            <div class="job-listing-location mb-3 mb-sm-0 custom-width w-25">
+                            <div class="job-listing-location mb-3 mb-sm-0 flex-w-25">
                                 <span class="icon-room"></span> New York, New York
                             </div>
-                            <div class="job-listing-meta">
+                            <div class="job-listing-meta mb-3 mb-sm-0">
                                 <span class="badge badge-danger"><?= $vacancyItem['type'] ?></span>
                             </div>
                         </div>
