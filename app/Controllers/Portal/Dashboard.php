9981d7625ff84b973d6b8c31aca6cd693378b86f
Wahyu Adi Candra <candraa6@gmail.com>
2020-11-22 19:16:43 +0700

    add some vacancy to dashboard

--- app/Controllers/Portal/Dashboard.php
+++ app/Controllers/Portal/Dashboard.php
@@ -7,12 +7,16 @@ use App\Libraries\TemplateEngine;
 use App\Views\Portal\Dashboard as PortalDashboard;
 use CodeIgniter\Controller;
 use App\Helpers\Auth;
+use App\Models\Portal\Vacancy as VacancyModel;
 
 /** @psalm-suppress PropertyNotSetInConstructor */
 class Dashboard extends Controller {
 
+    private $model;
+
     public function __construct()
     {
+        $this->model = new VacancyModel();
         helper(['view', 'auth']);
     }
 
@@ -21,7 +25,8 @@ class Dashboard extends Controller {
         return TemplateEngine::view(
             new PortalDashboard(
                 "Dashboard",
-                new User(Auth::user())
+                new User(Auth::user()),
+                $this->model->get()
             )
         );
     }
--- app/Views/Portal/Dashboard.php
+++ app/Views/Portal/Dashboard.php
@@ -1,8 +1,20 @@
 <?php namespace App\Views\Portal;
 
+use App\Entities\Typing\User;
+use App\Libraries\TemplateEngine;
+use App\Views\Portal\Partials\VacancyList;
 use App\Views\ViewHelper;
+use Config\RoutesConstant;
 
 class Dashboard extends AppShell {
+
+    private array $vacancy;
+
+    public function __construct(string $htmlTitle, User $user, array $vacancy)
+    {
+        parent::__construct($htmlTitle, $user);
+        $this->vacancy = $vacancy;
+    }
 
     protected function sectionContent(): string
     {
@@ -15,30 +27,11 @@ class Dashboard extends AppShell {
                         <h3 class="text-dark font-weight-bold mb-2">Hi, welcome back!</h3>
                         <h6 class="font-weight-normal mb-2">Last login was 23 hours ago. View details</h6>
                     </div>
-                    <div class="ml-lg-5 d-lg-flex d-none">
-                        <button type="button" class="btn bg-white btn-icon">
-                            <i class="mdi mdi-view-grid text-success"></i>
-                        </button>
-                        <button type="button" class="btn bg-white btn-icon ml-2">
-                            <i class="mdi mdi-format-list-bulleted font-weight-bold text-primary"></i>
-                        </button>
-                    </div>
                 </div>
             </div>
             <div class="col-sm-6">
                 <div class="d-flex align-items-center justify-content-md-end">
-                    <div class="pr-1 mb-3 mb-xl-0">
-                        <button type="button" class="btn btn-outline-inverse-info btn-icon-text">
-                            Feedback
-                            <i class="mdi mdi-message-outline btn-icon-append"></i>
-                        </button>
-                    </div>
-                    <div class="pr-1 mb-3 mb-xl-0">
-                        <button type="button" class="btn btn-outline-inverse-info btn-icon-text">
-                            Help
-                            <i class="mdi mdi-help-circle-outline btn-icon-append"></i>
-                        </button>
-                    </div>
+                    
                     <div class="pr-1 mb-3 mb-xl-0">
                         <button type="button" class="btn btn-outline-inverse-info btn-icon-text">
                             Print
@@ -49,74 +42,48 @@ class Dashboard extends AppShell {
             </div>
         </div>
         <div class="row mt-4">
-            <div class="col-lg-8 grid-margin stretch-card">
+            <div class="col-lg-8 grid-margin">
                 <div class="card">
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-lg-4">
-                                <h4 class="card-title">Sales Difference</h4>
-                                <canvas id="salesDifference"></canvas>
-                                <p class="mt-3 mb-4 mb-lg-0">Lorem ipsum dolor sit amet,
-                                    consectetur adipisicing elit.
-                                </p>
+                            <div class="col-lg-12">
+                                <h4 class="card-title">Vacancy List</h4>
+                                <?= TemplateEngine::view(new VacancyList($this->vacancy, 3)) ?>
                             </div>
-                            <div class="col-lg-5">
-                                <h4 class="card-title">Best Sellers</h4>
-                                <div class="row">
-                                    <div class="col-sm-4">
-                                        <ul class="graphl-legend-rectangle">
-                                            <li><span class="bg-danger"></span>Automotive</li>
-                                            <li><span class="bg-warning"></span>Books</li>
-                                            <li><span class="bg-info"></span>Software</li>
-                                            <li><span class="bg-success"></span>Video games</li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-sm-8 grid-margin">
-                                        <canvas id="bestSellers"></canvas>
-                                    </div>
-                                </div>
-                                <p class="mt-3 mb-4 mb-lg-0">Lorem ipsum dolor sit amet,
-                                    consectetur adipisicing elit.
-                                </p>
+
+                            <div class="col-lg-12 text-right">
+                                <b><a href="<?= route_to(RoutesConstant::VACANCY) ?>">See more...</a></b>
                             </div>
-                            <div class="col-lg-3">
-                                <h4 class="card-title">Social Media Statistics</h4>
-                                <div class="row">
-                                    <div class="col-sm-12">
-                                        <div class="progress progress-lg grouped mb-2">
-                                            <div class="progress-bar  bg-danger" role="progressbar" style="width: 40%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                                            <div class="progress-bar bg-info" role="progressbar" style="width: 10%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                                            <div class="progress-bar bg-warning" role="progressbar" style="width: 20%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                                            <div class="progress-bar bg-success" role="progressbar" style="width: 30%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-12">
-                                        <ul class="graphl-legend-rectangle">
-                                            <li><span class="bg-danger"></span>Instagram (15%)</li>
-                                            <li><span class="bg-warning"></span>Facebook (20%)</li>
-                                            <li><span class="bg-info"></span>Website (25%)</li>
-                                            <li><span class="bg-success"></span>Youtube (40%)</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <p class="mb-0 mt-2">Lorem ipsum dolor sit amet,
-                                    consectetur adipisicing elit.
-                                </p>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 mb-3 mb-lg-0">
-                <div class="card congratulation-bg text-center">
-                    <div class="card-body pb-0">
-                        <img src="<?= base_url(ViewHelper::PORTAL_ASSETS_RELATIVE_PATH . 'images/dashboard/face29.png') ?>" alt="">
-                        <h2 class="mt-3 text-white mb-3 font-weight-bold">Congratulations
-                            Johnson
-                        </h2>
-                        <p>You have done 57.6% more sales today.
-                            Check your new badge in your profile.
-                        </p>
+                <div class="row">
+                    <div class="col-lg-12 grid-margin">
+                        <div class="card ">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <h4 class="card-title">Pending Vacancy: </h4>
+                                        <?= TemplateEngine::view(new VacancyList($this->vacancy, 2)) ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 grid-margin">
+                        <div class="card ">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <h4 class="card-title">Saved Vacancy: </h4>
+                                        <?= TemplateEngine::view(new VacancyList([])) ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -261,7 +228,7 @@ class Dashboard extends AppShell {
                                             <div class="row">
                                                 <div class="col-sm-6 pl-lg-5">
                                                     <h2>$1635</h2>
-                                                    <p class="mb-0">Your Iincome</p>
+                                                    <p class="mb-0">Your Income</p>
                                                 </div>
                                                 <div class="col-sm-6 climate-info-border mt-lg-0 mt-2">
                                                     <h2>$2650</h2>
@@ -334,107 +301,7 @@ class Dashboard extends AppShell {
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-lg-2 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body pb-0">
-                        <div class="d-flex align-items-center justify-content-between">
-                            <h2 class="text-success font-weight-bold">18390</h2>
-                            <i class="mdi mdi-account-outline mdi-18px text-dark"></i>
-                        </div>
-                    </div>
-                    <canvas id="newClient"></canvas>
-                    <div class="line-chart-row-title">MY NEW CLIENTS</div>
-                </div>
-            </div>
-            <div class="col-lg-2 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body pb-0">
-                        <div class="d-flex align-items-center justify-content-between">
-                            <h2 class="text-danger font-weight-bold">839</h2>
-                            <i class="mdi mdi-refresh mdi-18px text-dark"></i>
-                        </div>
-                    </div>
-                    <canvas id="allProducts"></canvas>
-                    <div class="line-chart-row-title">All Products</div>
-                </div>
-            </div>
-            <div class="col-lg-2 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body pb-0">
-                        <div class="d-flex align-items-center justify-content-between">
-                            <h2 class="text-info font-weight-bold">244</h2>
-                            <i class="mdi mdi-file-document-outline mdi-18px text-dark"></i>
-                        </div>
-                    </div>
-                    <canvas id="invoices"></canvas>
-                    <div class="line-chart-row-title">NEW INVOICES</div>
-                </div>
-            </div>
-            <div class="col-lg-2 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body pb-0">
-                        <div class="d-flex align-items-center justify-content-between">
-                            <h2 class="text-warning font-weight-bold">3259</h2>
-                            <i class="mdi mdi-folder-outline mdi-18px text-dark"></i>
-                        </div>
-                    </div>
-                    <canvas id="projects"></canvas>
-                    <div class="line-chart-row-title">All PROJECTS</div>
-                </div>
-            </div>
-            <div class="col-lg-2 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body pb-0">
-                        <div class="d-flex align-items-center justify-content-between">
-                            <h2 class="text-secondary font-weight-bold">586</h2>
-                            <i class="mdi mdi-cart-outline mdi-18px text-dark"></i>
-                        </div>
-                    </div>
-                    <canvas id="orderRecieved"></canvas>
-                    <div class="line-chart-row-title">Orders Received</div>
-                </div>
-            </div>
-            <div class="col-lg-2 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body pb-0">
-                        <div class="d-flex align-items-center justify-content-between">
-                            <h2 class="text-dark font-weight-bold">7826</h2>
-                            <i class="mdi mdi-cash text-dark mdi-18px"></i>
-                        </div>
-                    </div>
-                    <canvas id="transactions"></canvas>
-                    <div class="line-chart-row-title">TRANSACTIONS</div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-6 grid-margin grid-margin-md-0 stretch-card">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center justify-content-between">
-                            <h4 class="card-title">Support Tracker</h4>
-                            <h4 class="text-success font-weight-bold">Tickets<span class="text-dark ml-3">163</span></h4>
-                        </div>
-                        <div id="support-tracker-legend" class="support-tracker-legend"></div>
-                        <canvas id="supportTracker"></canvas>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 grid-margin grid-margin-md-0 stretch-card">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="d-lg-flex align-items-center justify-content-between mb-4">
-                            <h4 class="card-title">Product Orders</h4>
-                            <p class="text-dark">+5.2% vs last 7 days</p>
-                        </div>
-                        <div class="product-order-wrap padding-reduced">
-                            <div id="productorder-gage" class="gauge productorder-gage"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
         </div>
         <!-- content-wrapper ends -->
         <!-- partial:partials/_footer.html -->
